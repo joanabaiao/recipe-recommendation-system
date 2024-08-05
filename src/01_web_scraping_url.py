5be866999ef9e01bd1e0d0169b236ab535edf398
joanabaiao <joanabaiao99@gmail.com>
2024-08-05 12:18:23 +0100

    Organized code with functions

--- src/01_web_scraping_url.py
+++ src/01_web_scraping_url.py
@@ -3,57 +3,59 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 import os
-
 from constants import *
+
 
 def get_html_content(url, save=False):
     response = requests.get(url)
     if response.status_code == 200:
-        if save: 
+        if save:
             file_path = os.path.join(HTML_DIR, "website_content.html")
-            with open(file_path, 'w', encoding='utf-8') as f:
+            with open(file_path, "w", encoding="utf-8") as f:
                 f.write(response.text)
                 print(f"Recipes HTML page saved to {file_path}")
-                    
+
         return response.content
     else:
         print(f"Failed to retrieve content. Status code: {response.status_code}")
         return None
 
+
 def extract_recipe_urls(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    script_tags = soup.find_all('script', type='application/ld+json')
-    
+    soup = BeautifulSoup(html_content, "html.parser")
+    script_tags = soup.find_all("script", type="application/ld+json")
+
     recipe_urls = []
-    
+
     for tag in script_tags:
         try:
             json_data = json.loads(tag.string)
-            if json_data.get('@type') == 'ItemList':
-                for item in json_data['itemListElement']:
-                    if item.get('@type') == 'ListItem' and 'url' in item:
-                        recipe_urls.append(item['url'])
+            if json_data.get("@type") == "ItemList":
+                for item in json_data["itemListElement"]:
+                    if item.get("@type") == "ListItem" and "url" in item:
+                        recipe_urls.append(item["url"])
         except (json.JSONDecodeError, KeyError):
             continue
-    
+
     return recipe_urls
 
-def save_urls_to_file(urls):
-    file_path = os.path.join(RAW_DATA_DIR, "recipe_urls.csv")
-    
-    df = pd.DataFrame({'recipe_urls': urls})
-    df.to_csv(file_path, index=False, encoding='utf-8')
+
+def save_urls_to_file(urls, file_path):
+    df = pd.DataFrame({"recipe_urls": urls})
+    df.to_csv(file_path, index=False, encoding="utf-8")
     print(f"URLs saved to {file_path}")
-    
-    
+
+
 ################################################################
+
 
 def main():
     html_content = get_html_content(BASE_URL, save=True)
-    
+
     if html_content:
         recipe_urls = extract_recipe_urls(html_content)
-        save_urls_to_file(recipe_urls)
+        save_urls_to_file(recipe_urls, file_path=URLS_PATH)
+
 
 if __name__ == "__main__":
-        main()
+    main()
--- src/02_web_scraping_recipe.py
+++ src/02_web_scraping_recipe.py
@@ -7,33 +7,40 @@ import time
 import os
 import pickle
 
-from utils.recipe_scraper_class import RecipeScraper 
+from utils.recipe_scraper_class import RecipeScraper
 from constants import *
 
-urls_file_path = os.path.join(RAW_DATA_DIR, "recipe_urls.csv")
-    
-recipe_urls = pd.read_csv(urls_file_path)
-print(f"Number of recipes: {len(recipe_urls)}")
 
-# Get data for all the links
-recipe_data = []
-for idx, url in enumerate(recipe_urls['recipe_urls'], start=1):
-    print(f"Processing recipe {idx}: {url}")
-    recipe_scraper = RecipeScraper(url)
-    recipe_scraper.get_recipe_info()
-    recipe_data.append(recipe_scraper.to_dict())
-    
-    # Sleep for 10 minutes every 400 requests
-    if idx % 400 == 0:
-        print("\nReached 400 requests. Sleeping for 10 minutes...\n")
-        time.sleep(600)  # Sleep for 600 seconds (10 minutes)
+def get_recipes_details(recipe_urls):
 
-recipe_info_df = pd.DataFrame(recipe_data)
+    recipe_data = []
+    for idx, url in enumerate(recipe_urls["recipe_urls"], start=1):
+        print(f"Processing recipe {idx}: {url}")
+        recipe_scraper = RecipeScraper(url)
+        recipe_scraper.get_recipe_info()
+        recipe_data.append(recipe_scraper.to_dict())
 
-with open('df.pkl', 'wb') as f:
-    pickle.dump(recipe_info_df, f)
-    
-# Save data
-file_path="data/raw/recipes_info.xlsx"
-recipe_info_df.to_excel(file_path, index=False)
-print(f"URLs saved to {file_path}")
+        # Sleep for 10 minutes every 400 requests
+        if idx % 400 == 0:
+            print("\nReached 400 requests. Sleeping for 10 minutes...\n")
+            time.sleep(600)  # Sleep for 600 seconds (10 minutes)
+
+    df_recipes_info = pd.DataFrame(recipe_data)
+
+    return df_recipes_info
+
+
+################################################################
+
+
+def main():
+    recipe_urls = pd.read_csv(URLS_PATH)
+    print(f"Number of recipes: {len(recipe_urls)}")
+
+    df_recipes = get_recipes_details(recipe_urls)
+    df_recipes.to_excel(RAW_RECIPES_PATH, index=False)
+    print(f"Recipes details successfully saved to: {RAW_RECIPES_PATH}")
+
+
+if __name__ == "__main__":
+    main()
